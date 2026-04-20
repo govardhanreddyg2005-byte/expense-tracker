6883cd0024212d93381affc948e5e33a038c8027
govardhan <govardhanreddy.g2005@gmail.com>
2026-04-20 10:18:56 +0530

    feat: add category wise total calculation

--- expense_tracker.py
+++ expense_tracker.py
@@ -22,3 +22,18 @@ def View_Expense(user_category):
         print('-'*32)
         expense_list = '\n'.join([f'{c:<12} {a:<12} {d}' for c,d,a in expense_record if user_category == c])
         return expense_list
+    
+
+def Category_Wise_Total():
+    if not expense_record:
+        return 'No Expenses!' 
+    category_total = {}
+
+    for category,date,amount in expense_record:
+        category_total[category] = category_total.get(category,0)+amount
+
+    print('--------CATEGORY WISE TOTAL--------')
+    print(f"{'Category':<10} {'Amount':<12}")
+    print('-'*22)
+    expenses = '\n'.join([f'{category:<10} {amount:<12}' for category,amount in category_total.items()])
+    return expenses
