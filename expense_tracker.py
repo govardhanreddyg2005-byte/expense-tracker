3d90c350f679d3473a0cf3908ce82077fde38eb2
govardhan <govardhanreddy.g2005@gmail.com>
2026-04-20 10:16:58 +0530

    feat: add view_expense function

--- expense_tracker.py
+++ expense_tracker.py
@@ -12,3 +12,13 @@ def Add_Expense(Category,Date,Amount):
     expense_record.append((Category,Amount,Date))
     print(f"{'Category':<10} {'Amount':<12} {'Date':<10}")
     return '-'*32
+
+
+def View_Expense(user_category):
+    if not expense_record:
+        return 'Empty Record'
+    else:
+        print(f"{'Category':<10} {'Amount':<12} {'Date':<10}")
+        print('-'*32)
+        expense_list = '\n'.join([f'{c:<12} {a:<12} {d}' for c,d,a in expense_record if user_category == c])
+        return expense_list
